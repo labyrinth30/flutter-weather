d0c1a63e1c69feaa453a93844d491da284127455
younha00 <ayeon0@office.skhu.ac.kr>
2023-10-31 20:00:39 +0900

    Lottie 추가

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:openweathermap/models/weather_model.dart';
 import 'package:openweathermap/secrets/secret.dart';
 import 'package:openweathermap/services/weather_service.dart';
@@ -32,6 +33,29 @@ class _WeatherScreenState extends State<WeatherScreen> {
   }
 
   // weather animaitons
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'frog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
 
   // initState
   @override
@@ -44,14 +68,24 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[500],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // city name
             Text(_weather?.cityName ?? 'loading city...'),
+
+            //animation
+            Lottie.asset(
+              getWeatherAnimation(_weather?.mainCondition),
+            ),
+
             // temperature
             Text('${_weather?.temperature.round()} °C'),
+            // weather condition
+
+            Text(_weather?.mainCondition ?? ""),
           ],
         ),
       ),
